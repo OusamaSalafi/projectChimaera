3a9ec20845cb8913cbe82104287d78f6c777a423
GarethG <info@gareth-griffiths.co.uk>
2012-03-24 15:07:21 +0000

    Added a tutorial package for the /tf package, we need this to tell the sub the position of its sensors relative to its mass center point.

--- Soft_Nodes/sonarSpammer/src/sonarSpammer.cpp
+++ Soft_Nodes/sonarSpammer/src/sonarSpammer.cpp
@@ -1,3 +1,7 @@
+//Useful shit I found
+//http://ros.informatik.uni-freiburg.de/roswiki/navigation(2f)Tutorials(2f)RobotSetup(2f)Sensors.html
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,6 +13,8 @@
 #include "std_msgs/Int32MultiArray.h"
 #include "std_msgs/Int32.h"
 #include "std_msgs/Float32.h"
+//+++
+#include "sensor_msgs/LaserScan.h"
 
 int main(int argc, char **argv)
 {
@@ -26,22 +32,53 @@ int main(int argc, char **argv)
 	std_msgs::Int32MultiArray sonarBinsArr;
 	std_msgs::Float32 sonarBearing;
 
+	//laser scan publisher and dec
+	ros::Publisher sonarScanMsg = n.advertise<sensor_msgs::LaserScan>("sonarScan", 100);
+	sensor_msgs::LaserScan sonarScan;
+
+
+	//laser scanner config
+	double sonar_frequency = 2000; //same as usleep???
+	int num_sonar_readings = 6399; //http://answers.ros.org/question/12381/time-issue-when-publishing-laser-scan-message
+
+	sonarScan.header.frame_id = "sonar"; 
+
+	sonarScan.angle_min = -0.00098125; //see SLAM wiki	
+	sonarScan.angle_max = 0.00098125; //see SLAM wiki
+	sonarScan.angle_increment = 0.00098125; //see SLAM wiki - I'm not sure about this one, or are the above the same to allow for a 360 min and max angle?
+
+	sonarScan.time_increment = (1 / sonar_frequency) / (num_sonar_readings); //see link on num_sonar_readings
+
+	sonarScan.range_min = 0.833333333;
+	sonarScan.range_max = 75;	
+
+	ros::Time scan_time = ros::Time::now();
+	
 	
 	while (ros::ok())
 	{
-		if(bearing == 6399.0)
+		if(bearing == 6399)//6399.0)
+		{
 			bearing = 0.0;
-			
+			//printf("Done!\n");
+			//return 0;
+		}		
+	
 		sonarBinsArr.data.clear();
 		for (int i = 0; i < 90; i++)
 		{
 			sonarBinsArr.data.push_back(rand() % 255);
+			sonarScan.intensities.push_back(rand() % 255);
 		}
+
+		sonarScan.header.stamp = scan_time; //this seems to allow in index of the scans 
 		sonarBearing.data = bearing;
 
 		pub.publish(sonarBinsArr);
 		sonarBearingMsg.publish(sonarBearing);
 		
+		sonarScanMsg.publish(sonarScan);
+
 		printf("%f \n", bearing);
 		
 		ros::spinOnce();
