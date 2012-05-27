02744da4a03433a2895e3833a08dec958da3d918
P-LAN <Patrick2.Lancaster@live.uwe.ac.uk>
2012-05-27 16:52:34 +0100

    Added video and image input modes for testing purposes.

--- Drivers/dwncam2/src/dwncamDriver.cpp
+++ Drivers/dwncam2/src/dwncamDriver.cpp
@@ -33,32 +33,33 @@ class Demo
 {
 
 protected:
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  sensor_msgs::CvBridge bridge_;
-  cv::Mat img_in_;
-  cv::Mat img_hsv_;
-  cv::Mat img_hue_;
-  cv::Mat img_sat_;
-  cv::Mat img_bin_;
-  cv::Mat img_out_;
-  IplImage *cv_input_;
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	sensor_msgs::CvBridge bridge_;
+	cv::Mat img_in_;
+	cv::Mat img_hsv_;
+	cv::Mat img_hue_;
+	cv::Mat img_sat_;
+	cv::Mat img_bin_;
+	cv::Mat img_out_;
+	IplImage *cv_input_;
 
 public:
 
-    Demo (ros::NodeHandle & nh):nh_ (nh), it_ (nh_)
-  {
-    ROS_INFO("Getting Cam");
-    // Listen for image messages on a topic and setup callback
-    image_sub_ = it_.subscribe ("/gscam/image_raw", 1, &Demo::imageCallback, this);
-    // Open HighGUI Window
-    ROS_INFO("Got Cam");
-    cv::namedWindow ("input", 1);
-    cv::namedWindow ("binary image", 1);
-    cv::namedWindow ("segmented output", 1);
-    ROS_INFO("Opened window?");
-  }
+	Demo(ros::NodeHandle & nh):nh_ (nh), it_ (nh_)
+	{
+		ROS_INFO("Getting Cam");
+		// Listen for image messages on a topic and setup callback
+		image_sub_ = it_.subscribe ("/gscam/image_raw", 1, &Demo::imageCallback, this);
+		// Open HighGUI Window
+		ROS_INFO("Got Cam");
+		cv::namedWindow ("input", 1);
+		cv::namedWindow ("binary image", 1);
+		cv::namedWindow ("segmented output", 1);
+		ROS_INFO("Opened window?");
+	}
+
 
 	void findCentre(void)
 	{
@@ -127,84 +128,130 @@ public:
 		return;
 	}
 
-  void imageCallback (const sensor_msgs::ImageConstPtr & msg_ptr)
-  {
-    // Convert ROS Imput Image Message to IplImage
-    try
-    {
-      cv_input_ = bridge_.imgMsgToCv (msg_ptr, "bgr8");
-    }
-    catch (sensor_msgs::CvBridgeException error)
-    {
-      ROS_ERROR ("CvBridge Input Error");
-    }
 
-    // Convert IplImage to cv::Mat
-    img_in_ = cv::Mat (cv_input_).clone ();
-    // output = input
-    img_out_ = img_in_.clone ();
-    // Convert Input image from BGR to HSV
-    cv::cvtColor (img_in_, img_hsv_, CV_BGR2HSV);
-    // Zero Matrices
-    img_hue_ = cv::Mat::zeros(img_hsv_.rows, img_hsv_.cols, CV_8U);
-    img_sat_ = cv::Mat::zeros(img_hsv_.rows, img_hsv_.cols, CV_8U);
-    img_bin_ = cv::Mat::zeros(img_hsv_.rows, img_hsv_.cols, CV_8U);
-    // HSV Channel 0 -> img_hue_ & HSV Channel 1 -> img_sat_
-    int from_to[] = { 0,0, 1,1};
-    cv::Mat img_split[] = { img_hue_, img_sat_};
-    cv::mixChannels(&img_hsv_, 3,img_split,2,from_to,2);
+	void imageCallback(const sensor_msgs::ImageConstPtr & msg_ptr)
+	{
 
-    for(int i = 0; i < img_out_.rows; i++)
-    {
-      for(int j = 0; j < img_out_.cols; j++)
-      {
-        // The output pixel is white if the input pixel
-        // hue is orange and saturation is reasonable
-        if(img_hue_.at<uchar>(i,j) > 4 &&
-           img_hue_.at<uchar>(i,j) < 30 &&
-           img_sat_.at<uchar>(i,j) > 126) {
-          img_bin_.at<uchar>(i,j) = 255;
-        } else {
-          img_bin_.at<uchar>(i,j) = 0;
-          // Clear pixel blue output channel
-          img_out_.at<uchar>(i,j*3+0) = 0;
-          // Clear pixel green output channel
-          img_out_.at<uchar>(i,j*3+1) = 0;
-          // Clear pixel red output channel
-          img_out_.at<uchar>(i,j*3+2) = 0;
-        }
-      }
-    }
+		// Convert ROS Imput Image Message to IplImage
+		try
+		{
+			cv_input_ = bridge_.imgMsgToCv (msg_ptr, "bgr8");
+ 		}
+		catch (sensor_msgs::CvBridgeException error)
+		{
+			ROS_ERROR ("CvBridge Input Error");
+ 		}
 
-	findCentre();
+		// Convert IplImage to cv::Mat
+		img_in_ = cv::Mat (cv_input_).clone ();
+		// output = input
+		img_out_ = img_in_.clone ();
+		// Convert Input image from BGR to HSV
+		cv::cvtColor (img_in_, img_hsv_, CV_BGR2HSV);
+		// Zero Matrices
+		img_hue_ = cv::Mat::zeros(img_hsv_.rows, img_hsv_.cols, CV_8U);
+		img_sat_ = cv::Mat::zeros(img_hsv_.rows, img_hsv_.cols, CV_8U);
+		img_bin_ = cv::Mat::zeros(img_hsv_.rows, img_hsv_.cols, CV_8U);
+		// HSV Channel 0 -> img_hue_ & HSV Channel 1 -> img_sat_
+		int from_to[] = { 0,0, 1,1};
+		cv::Mat img_split[] = { img_hue_, img_sat_};
+		cv::mixChannels(&img_hsv_, 3,img_split,2,from_to,2);
+
+		for(int i = 0; i < img_out_.rows; i++)
+		{
+			for(int j = 0; j < img_out_.cols; j++)
+			{
+  				// The output pixel is white if the input pixel
+				// hue is orange and saturation is reasonable
+				if(img_hue_.at<uchar>(i,j) > 25 && //4
+				img_hue_.at<uchar>(i,j) < 34 && //30
+				img_sat_.at<uchar>(i,j) > 126){
+					img_bin_.at<uchar>(i,j) = 255;
+				} else {
+					img_bin_.at<uchar>(i,j) = 0;
+					// Clear pixel blue output channel
+					img_out_.at<uchar>(i,j*3+0) = 0;
+					// Clear pixel green output channel
+					img_out_.at<uchar>(i,j*3+1) = 0;
+					// Clear pixel red output channel
+					img_out_.at<uchar>(i,j*3+2) = 0;
+				}
+			}
+		}
+
+		findCentre();
 	
-    // Display Input image
-    cv::imshow ("input", img_in_);
-    // Display Binary Image
-    cv::imshow ("binary image", img_bin_);
-    // Display segmented image
-    cv::imshow ("segmented output", img_out_);
+		// Display Input image
+		cv::imshow ("input", img_in_);
+		// Display Binary Image
+		cv::imshow ("binary image", img_bin_);
+		// Display segmented image
+		cv::imshow ("segmented output", img_out_);
 
-    // Needed to  keep the HighGUI window open
-    cv::waitKey (3);
-  }
+		// Needed to  keep the HighGUI window open
+		cv::waitKey (3);
+
+	}
+
 
 };
 
 
-	
-int
-main (int argc, char **argv)
+
+int main(int argc, char **argv)
 {
-  // Initialize ROS Node
-  ros::init (argc, argv, "ihr_demo1");
-  // Start node and create a Node Handle
-  ros::NodeHandle nh;
-  // Instaniate Demo Object
-  ROS_INFO("Online");
-  Demo d (nh);
-  // Spin ...
-  ros::spin ();
-  // ... until done
-  return 0;
+ 	// Initialize ROS Node
+ 	ros::init(argc, argv, "ihr_demo1");
+	// Start node and create a Node Handle
+	ros::NodeHandle nh;
+ 	// Instaniate Demo Object
+	ROS_INFO("Online");
+	Demo d(nh);
+  	// Spin ...
+
+
+
+	/* Leave One Section Uncommented And The Others Commented Out */
+	/* Replace Video/Image Filename If Necessary (Files Should Be Stored In The "dwncam2" Folder) */
+
+
+	/****************** FOR CAMERA INPUT MODE ******************/
+  	//ros::spin();
+	/***********************************************************/
+
+
+	/****************** FOR VIDEO INPUT MODE ******************/
+	cv::Mat video_frame;
+	IplImage video_frame2;
+	sensor_msgs::CvBridge bridge2_;
+	cv::VideoCapture cap("Comp.avi");
+    	if(!cap.isOpened()){
+        	return -1;
+	}
+	while(ros::ok()){
+		ros::spinOnce();
+		cap >> video_frame;
+		video_frame2 = video_frame;
+		d.imageCallback(bridge2_.cvToImgMsg(&video_frame2, "passthrough"));
+	}
+	/**********************************************************/
+
+
+	/****************** FOR IMAGE INPUT MODE ******************/
+	/*cv::Mat video_frame;
+	IplImage video_frame2;
+	sensor_msgs::CvBridge bridge2_;
+	while(ros::ok()){
+		ros::spinOnce();
+		video_frame = cvLoadImage("a.jpg", CV_LOAD_IMAGE_COLOR);
+		video_frame2 = video_frame;
+		d.imageCallback(bridge2_.cvToImgMsg(&video_frame2, "passthrough"));
+	}*/
+	/**********************************************************/
+
+
+
+	// ... until done
+	return 0;
 }
+
