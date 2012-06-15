31f529b1cc67605a6bf7817701f51558132a4a88
Alex Sleat <alexsleat@gmail.com>
2012-06-15 08:22:26 +0100

    sonarDriver.cpp is from fitpc, .bkup is from laptop, should be about the same but with differing settings.

--- Soft_Nodes/sonarScanner/src/sonarScanner.h
+++ Soft_Nodes/sonarScanner/src/sonarScanner.h
@@ -1,3 +1,4 @@
+#define THRESHOLD 207
 
 float bins;
 float bearing;
@@ -7,6 +8,7 @@ void pixelPlace( float theta, unsigned int distance, unsigned int depth);
 int genRand( int n );
 void printascii( void );
 void drawScene(unsigned int x, unsigned int y, unsigned int depth);
+void initGL(int width, int height);
 void bearingCallback(const std_msgs::Float32::ConstPtr& sonarBearing);
 void binsCallback(const std_msgs::Float32::ConstPtr& sonarBins);
 void binsArrCallback(const std_msgs::Int32MultiArray::ConstPtr& sonarBinsArr);
