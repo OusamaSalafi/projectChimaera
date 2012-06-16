363affd13269fd84866161ecda9604643b1987d8
Alex Sleat <alexsleat@gmail.com>
2012-06-16 15:42:22 +0100

    also added latching to other publishers.

--- Drivers/motor/src/spammer.cpp
+++ Drivers/motor/src/spammer.cpp
@@ -19,49 +19,44 @@ ros::Publisher p_yaw_left;
 ros::Publisher p_pitch;
 ros::Publisher p_go;
 
+std_msgs::UInt32 msgDR;
+std_msgs::UInt32 msgDL;
+std_msgs::UInt32 msgYR;
+std_msgs::UInt32 msgYL;
+std_msgs::UInt32 msgP;
+std_msgs::UInt32 msgGO;
+
 //callback functions for each slider
 
 void depth_right_cb(int pos)
 {
-	std_msgs::UInt32 msg;
-	msg.data = pos+1000;
-	p_depth_right.publish(msg);
+	msgDR.data = pos+1000;
 }
 
 void depth_left_cb(int pos)
 {
-	std_msgs::UInt32 msg;
-	msg.data = pos+1000;
-	p_depth_left.publish(msg);
+	msgDL.data = pos+1000;
 }
 
 void yaw_right_cb(int pos)
 {
-	std_msgs::UInt32 msg;
-	msg.data = pos+1000;
-	p_yaw_right.publish(msg);
+	msgYR.data = pos+1000;
 }
 
 void yaw_left_cb(int pos)
 {
-	std_msgs::UInt32 msg;
-	msg.data = pos+1000;
-	p_yaw_left.publish(msg);
+	msgYL.data = pos+1000;
 }
 
 void pitch_cb(int pos)
 {
-	std_msgs::UInt32 msg;
-	msg.data = pos+1000;
-	p_pitch.publish(msg);
+	msgP.data = pos+1000;
 }
 
 
 void go_cb(int pos)
 {
-	std_msgs::UInt32 msg;
-	msg.data = pos;
-	p_go.publish(msg);
+	msgGO.data = pos;
 }
 
 int main(int argc, char **argv)
@@ -92,14 +87,20 @@ int main(int argc, char **argv)
 	
 	cvCreateTrackbar("GO", "MotorSpammer", &go, 1, &go_cb);
 
+	msgDR.data = msgDL.data = msgYR.data = msgYL.data = msgP.data = 1500;
+
 	while (ros::ok())
 	{
 
 		// Allow The HighGUI Windows To Stay Open
 		cv::waitKey(3);
-			std_msgs::UInt32 msg;
-			msg.data = go;
-			p_go.publish(msg);
+			
+			p_depth_right.publish(msgDR);
+			p_depth_left.publish(msgDL);
+			p_yaw_right.publish(msgYR);
+			p_yaw_left.publish(msgYL);
+			p_pitch.publish(msgP);
+			p_go.publish(msgGO);
 	
 	}
 }
